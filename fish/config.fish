8565db23cf4425ff6d434247a18c5231212f1c97
BodeSpezial <konstantin.bode@outlook.com>
2020-12-26 02:31:12 +0100

    set some variables for npm and haskell

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,9 @@ thefuck --alias duck | source
 # bat to color manpages
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
+set -g fish_user_paths "/usr/local/opt/node@12/bin" $fish_user_paths
+set -g fish_user_paths "/Users/konstatin/.local/bin" $fish_user_paths
+
+# ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+test -f /Users/konstatin/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /Users/konstatin/.ghcup/bin $PATH
