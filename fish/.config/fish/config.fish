a3330a3c0b502d653a55eca793a9766ad231287b
BodeSpezial <konstantin.bode@outlook.com>
2021-04-07 00:28:33 +0200

    Make the preview of cd colored

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,3 +13,7 @@ set -g fish_user_paths "/Users/konstatin/.local/bin" $fish_user_paths
 # ghcup-env
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 test -f /Users/konstatin/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /Users/konstatin/.ghcup/bin $PATH
+
+# make the preview of `cd` colored and sorted
+set -gx FZF_PREVIEW_DIR_CMD "exa --color=always -s extension"
+
